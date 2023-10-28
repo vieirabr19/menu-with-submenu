39b55a8556874fbce5a49dd94c6210f1165f9ee8
Luiz Vieira <luiz22_sp@hotmail.com>
2023-10-28 20:23:17 -0300

    Alteração no JavaScript

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const aside = document.querySelector("aside");
 const asideToggle = document.querySelector(".menu-icon");
-const navItem = aside.getElementsByClassName("nav-item");
+const navItem = aside.querySelectorAll(".nav-item");
 
 asideToggle.addEventListener("click", () => {
   aside.classList.toggle("expand");
 });
 
-for (let item of navItem) {
-  item.onclick = activeItem;
-}
+navItem.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.classList.contains("active")) {
+      button.classList.remove("active");
+    } else {
+      navItem.forEach((e) => e.classList.remove("active"));
+      button.classList.add("active");
+    }
+  });
+});
 
-function activeItem() {
-  const item = Array.from(navItem);
-  item.forEach((e) => e.classList.remove("active"));
-  this.classList.add("active");
-}
+// for (let item of navItem) {
+//   item.onclick = activeItem;
+// }
+
+// function activeItem() {
+//   const item = Array.from(navItem);
+//   item.forEach((e) => e.classList.remove("active"));
+//   this.classList.add("active");
+// }
